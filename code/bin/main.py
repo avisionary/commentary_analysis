0731883d4c6b00461d7f2057eeaae700298f1f2c
Avisionary <aa2464@georgetown.edu>
2023-04-23 18:02:59 -0400

    Setting structure

---
+++ code/bin/main.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description="Code to make corpus "
+    #                                              "for Naive Bayes homework")
+    # parser.add_argument("-f", "--indir", required=True, help="Data directory")
+    # args = parser.parse_args()
+
+    
+
+  
---
+++ code/tests/test_code.py
@@ -0,0 +1,3 @@
+import os
+import numpy as np
+import regex as re
